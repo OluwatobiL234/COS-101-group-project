14c516b572f809f1675417a4e50e98aa11f4f497
philipamfani <150629116+philipamfani@users.noreply.github.com>
2024-02-06 00:10:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ import tkinter as tk
 class CalculatorApp:
     def __init__(self, root):
         self.root = root
+        self.root.title("MARKET CALCULATOR")
+
+        self.entry_var = tk.StringVar()
+        self.entry = tk.Entry(root, textvariable=self.entry_var, font=('Bahnschrift SemiLight Condensed', 25), justify='right', bd=10)
+        self.entry.grid(row=0, column=0, columnspan=4)
+
+        buttons = [
+            '1', '2', '3', '+',
+            '4', '5', '6', '*',
+            '7', '8', '9', '-',
+            '0', '.', '/', '='
+        ]
     def button_click(self, value):
         current_expression = self.entry_var.get()
 
